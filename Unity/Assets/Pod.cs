225f9a5201f3874e45a59f18e52f8ee0b0f9fb17
declanqc <C20465442@mytudublin.ie>
2023-04-25 10:33:09 +0100

    added pod entering functionality

--- Unity/Assets/Pod.cs
+++ Unity/Assets/Pod.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class Pod : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject Boid;
 
+    public FPSController PlayerControls;
+    public GameObject Player;
+    public GameObject PlayerCamera;
+    public GameObject Boid;
+    public Vector3 Camholder = Vector3.zero;
+    public bool InPod = false;
 
 
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("entered");
-        if(other.gameObject.CompareTag("Player"))
+        if (other.GetComponent<FPSController>())
         {
             Debug.Log("entered");
+            InPod = true;
         }
     }
 
     public void Update()
     {
         
+        if(InPod == true)
+        {
+            Debug.Log("InPod");
+            Player.transform.position = Boid.transform.position;
+            Player.transform.rotation = Boid.transform.rotation;
+            PlayerCamera.transform.position = Boid.transform.position;
+            
+            PlayerCamera.transform.rotation = Boid.transform.rotation;
+            Camholder = PlayerCamera.transform.position;
+            Camholder.y += 20f;
+
+        }
     }
 
 
